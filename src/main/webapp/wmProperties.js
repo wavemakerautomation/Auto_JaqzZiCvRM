64c33917cdce8669ab9f6661443f2709058d2193
EuwewptcML <appTest4@wavemaker.com>
2017-11-28 22:49:16 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_JaqzZiCvRM",
+  "homePage" : "Main",
+  "name" : "Auto_JaqzZiCvRM",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
